4d0d35c5cb0929b22973cd8d31742a3fc73078f2
ozansz <ozan.sazak@protonmail.ch>
2018-07-10 21:02:43 +0300

    HomePage initial impl

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,24 @@
 <template lang="html">
   <div id="app-page-home">
-
+    <app-component-postpre v-for="post, indx in posts" :post_data="post" :data-indx="indx"></app-component-postpre>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app-page-home'
+  name: 'app-page-home',
+  data: {
+    posts: []
+  },
+  created () {
+    var vm = this
+    $root.getAxios().get('/post')
+      .then(res => {
+        vm.posts = res.data.posts
+      }, err => {
+
+      })
+  }
 }
 </script>
 
