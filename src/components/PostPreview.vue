682751ab7038be543757f2e7f62b72f29b7e582d
ozansz <ozan.sazak@protonmail.ch>
2018-07-11 16:54:50 +0300

    PostPreview component pre fin

--- src/components/PostPreview.vue
+++ src/components/PostPreview.vue
@@ -1,6 +1,10 @@
 <template lang="html">
   <div class="app-component-postpre">
-    
+    <img v-if="post_data.use_img" :src="post_data.image_url">
+    <div class="pre-header">{{ post_data.title }}</div>
+    <div class="pre-ts">Posted by {{ post_data.author }} on {{ normalized_date }}</div>
+    <div class="pre-content">{{ normalized_text }}</div>
+    <router-link :to="'/post/' + post_data.id.toString()">Continue reading</router-link>
   </div>
 </template>
 
@@ -9,9 +13,65 @@ export default {
   name: 'app-component-postpre',
   props: [
     'post_data'
-  ]
+  ],
+  data () {
+    return {
+      normalized_date: undefined
+    }
+  },
+  watch: {
+    post_data (_new) {
+      this.normalize()
+    }
+  },
+  mounted () {
+    this.normalize()
+  },
+  methods: {
+    normalize () {
+      this.normalized_date = new Date(this.post_data.ts).toDateString()
+      if (this.post_data.text.length > 168) {
+        this.normalized_text = this.post_data.text.slice(0, 169) + ' ...'
+      }
+    }
+  }
 }
 </script>
 
 <style lang="css">
+.app-component-postpre {
+  position: relative;
+  text-align: center;
+  color: #27292B;
+  padding-bottom: 25px;
+}
+.app-component-postpre img {
+  width: 100%;
+  height: auto;
+}
+.app-component-postpre .pre-header {
+  font-family: 'Lora', serif;
+  font-size: 37px;
+  margin-top: 18px;
+  margin-bottom: 6px;
+}
+.app-component-postpre .pre-ts {
+  font-size: 14px;
+}
+.app-component-postpre .pre-content {
+  font-size: 16px;
+  width: 85%;
+  margin: 25px auto;
+}
+.app-component-postpre .pre-content, .app-component-postpre .pre-ts {
+  font-family: 'Lato', sans-serif;
+}
+.app-component-postpre a {
+  color: #27292B;
+}
+@media only screen and (min-width: 480px) {
+  .app-component-postpre .pre-header {
+    font-size: 51px;
+  }
+}
 </style>
